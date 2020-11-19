aa153949fe0e815dd301ebae6a48c5c6cb99ba45
Xavier gilbert <xavier.gilbert@food.gov.uk>
2020-11-19 14:46:28 +0000

    fix honorific title regex typo: word border generalized to all entities

--- R/name.R
+++ R/name.R
@@ -49,9 +49,9 @@ strip_business_legal_entity_type <- function(string, trim_ws = FALSE, squish_ws 
 #' @importFrom purrr when
 strip_honorific_title <- function(string, trim_ws = FALSE, squish_ws = TRUE) {
   pattern <- paste0(
-               "((\\b[MmSs][RSrs]{1,2}|[Mm]iss|[Dd][Rr][Ss]?|[Pp][Rr][Oo][Ff](essor)?)(\\.|\\b))",
+               "(\\b([MmSs][RSrs]{1,2}|[Mm]iss|[Dd][Rr][Ss]?|[Pp][Rr][Oo][Ff](essor)?)(\\.|\\b))",
                "(\\sand\\s|\\s&\\s)?",
-               "((\\b[MmSs][RSrs]{1,2}|[Mm]iss|[Dd][Rr][Ss]?|[Pp][Rr][Oo][Ff](essor)?)(\\.|\\b))?"
+               "(\\b([MmSs][RSrs]{1,2}|[Mm]iss|[Dd][Rr][Ss]?|[Pp][Rr][Oo][Ff](essor)?)(\\.|\\b))?"
              )
 
   string %>%
--- tests/testthat/test-name.R
+++ tests/testthat/test-name.R
@@ -19,3 +19,75 @@ test_that("strip entity type from name", {
     "Farmer Box"
   )
 })
+
+test_that("strip honorific title from name", {
+  titles <- c(
+    "mr and mrs",
+    "mr and ms",
+    "Ms and ms",
+    "mr and miss.",
+    "Mr Miss",
+    "mr & mrs",
+    " miss Mr ",
+    "Mr and Mrs and Miss Holmes",
+    "Mr Robert and Mrs Anna Wilson and Professor Holmes",
+    " and Mr ",
+    "Mr Andy",
+    "Mr and Holmes, Miss Enola Holmes",
+    "Mrs.",
+    "Hello World",
+    "Mr. Holmes",
+    "Miss Holmes Ms",
+    "M&S",
+    "Hussain",
+    "Private Drive Road",
+    "Mr Holmes missed his binoculars",
+    "Dr. Jekyll and Mr Hyde",
+    "DS Wrap",
+    "Denis",
+    "Drs Associates",
+    "Prof Holmes",
+    "Prof. Holmes",
+    "Professor Holmes",
+    "Tharaldr Coffee",
+    "Omr",
+    "Dismiss"
+  )
+  titles_stripped <-
+    c(
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "and Holmes",
+      "Robert and Anna Wilson and Holmes",
+      "and",
+      "Andy",
+      "Holmes, Enola Holmes",
+      "",
+      "Hello World",
+      "Holmes",
+      "Holmes",
+      "M&S",
+      "Hussain",
+      "Private Drive Road",
+      "Holmes missed his binoculars",
+      "Jekyll and Hyde",
+      "DS Wrap",
+      "Denis",
+      "Associates",
+      "Holmes",
+      "Holmes",
+      "Holmes",
+      "Tharaldr Coffee",
+      "Omr",
+      "Dismiss"
+    )
+  expect_equal(
+    strip_honorific_title(titles),
+    titles_stripped
+  )
+})
